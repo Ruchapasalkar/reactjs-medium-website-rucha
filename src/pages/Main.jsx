ab03981f1f3784b537e2629b470a886119f74ffd
Ruchapasalkar <ruchaapasalkar2508@gmail.com>
2025-01-16 15:08:41 +0530

    Saved local changes before pulling

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Main() {
   return(
     <div className="text-center p-8">
-      <h1 className="text-5xl font-serif font-bold leading-tight text-left">
+      <h1 className="text-8xl font-serif font-bold leading-tight text-left">
         Human <br />
         <span className="text-gray-900">Stories & ideas</span>
       </h1>
-      <p className="text-xl mt-4 text-left"> A place to read, write, and deepen your understanding</p>
+      <p className="text-4xl mt-4 text-left"> A place to read, write, and deepen your understanding</p>
       <button className="mt-8 px-6 py-3 bg-black text-white rounded-full text-lg origin-left">
         Start Reading
       </button>
