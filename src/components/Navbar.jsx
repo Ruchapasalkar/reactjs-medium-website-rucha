ef06b5aea7818f4aa5377c933d7db5ff1655e770
Ruchapasalkar <ruchaapasalkar2508@gmail.com>
2025-01-21 21:04:15 +0530

    Updated Hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
-    <nav className="bg-black text-white p-4 flex justify-between items-center font-bold">
+    <nav className="bg-black text-white p-4 flex justify-between items-center font-bold relative">
       <h1 className="text-2xl font-serif">Medium</h1>
-      <div className="flex space-x-4">
+      <button
+        className="hamburger lg:hidden"
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        ☰
+      </button>
+      <div className="hidden lg:flex space-x-4">
         <Link to="/our-story" className="hover:underline">
           Our Story
         </Link>
@@ -22,8 +30,26 @@ function Navbar() {
           Get Started
         </button>
       </div>
+      {isMobileMenuOpen && (
+        <div className="mobile-menu lg:hidden">
+          <Link to="/our-story" className="py-2 hover:underline">
+            Our Story
+          </Link>
+          <Link to="/membership" className="py-2 hover:underline">
+            Membership
+          </Link>
+          <Link to="/write" className="py-2 hover:underline">
+            Write
+          </Link>
+          <Link to="/sign-in" className="py-2 hover:underline">
+            Sign In
+          </Link>
+          <button className="mt-4 px-4 py-2 bg-white text-black rounded-full">
+            Get Started
+          </button>
+        </div>
+      )}
     </nav>
-
   );
 }
 
